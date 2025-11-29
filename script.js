9dc8bd34b41295fd8156efd3ddc594550e311394
Manav Verma <manavvrma17@gmail.com>
2025-11-29 21:36:21 +0530

    added wallpaper change button

--- script.js
+++ script.js
@@ -43,3 +43,60 @@ function deleteTask(event, btn) {
     li.remove();
   }, 300);
 }
+
+const wallpaperBtn = document.getElementById("wallpaperBtn");
+let currentTheme = 0;
+
+const themes = [
+  {
+    bg: "#f4f1ea",
+    image:
+      "radial-gradient(#b0aba0 0.5px, transparent 0.5px), radial-gradient(#b0aba0 0.5px, #f4f1ea 0.5px)",
+    size: "20px 20px",
+  },
+
+  {
+    bg: "#fdfbf7",
+    image:
+      "linear-gradient(#e0ddd5 1px, transparent 1px), linear-gradient(90deg, #e0ddd5 1px, transparent 1px)",
+    size: "20px 20px",
+  },
+
+  {
+    bg: "#ffffff",
+
+    image: "url('assets/1.jpg')",
+
+    size: "cover",
+  },
+  {
+    bg: "#ffffff",
+
+    image: "url('assets/2.jpg')",
+
+    size: "cover",
+  },
+  {
+    bg: "#ffffff",
+
+    image: "url('assets/3.jpg')",
+
+    size: "cover",
+  },
+  {
+    bg: "#ffffff",
+
+    image: "url('assets/4.jpg')",
+
+    size: "cover",
+  },
+];
+
+wallpaperBtn.addEventListener("click", () => {
+  currentTheme = (currentTheme + 1) % themes.length;
+  const theme = themes[currentTheme];
+
+  document.body.style.backgroundColor = theme.bg;
+  document.body.style.backgroundImage = theme.image;
+  document.body.style.backgroundSize = theme.size;
+});
